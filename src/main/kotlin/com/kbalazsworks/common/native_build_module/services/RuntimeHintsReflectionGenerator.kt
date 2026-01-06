e73b3070b51221c9de5f4ac1a635089252e96623
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-01-06 22:06:19 +0100

    RuntimeHintsReflectionGenerator kotlinize

--- src/main/kotlin/com/kbalazsworks/common/native_build_module/services/RuntimeHintsReflectionGenerator.kt
+++ src/main/kotlin/com/kbalazsworks/common/native_build_module/services/RuntimeHintsReflectionGenerator.kt
@@ -7,64 +7,43 @@ import com.kbalazsworks.common.templating_module.services.MustacheService
 import org.springframework.stereotype.Service
 import java.io.IOException
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 
 @Service
-class RuntimeHintsReflectionGenerator
-    (private val mustacheService: MustacheService, private val fileService: FileService) {
+class RuntimeHintsReflectionGenerator(
+    private val mustacheService: MustacheService,
+    private val fileService: FileService
+) {
+
     @Throws(IOException::class, RuntimeHintsReflection::class)
     fun generate(destinationFile: String, packageNames: List<String>, classes: List<Class<*>>) {
-        val classNames: List<String> = getClassNamesInPackages(packageNames, classes)
-
-        val newCode = mustacheService.execute(
-            "templates/RuntimeHintsReflectionTemplate.mustache",
-            ReflectionClassList(classNames)
-        )
+        val newCode = mustacheService
+            .execute(TEMPLATE, ReflectionClassList(getClassNamesInPackages(packageNames, classes)))
 
         val oldCode = fileService.readString(destinationFile)
+
         fileService.saveString(destinationFile, newCode)
 
-        if (oldCode != newCode) {
-            throw RuntimeHintsReflection("New ReflectionConfiguration generated, please restart the application!")
-        }
+        require(oldCode == newCode) { "New ReflectionConfiguration generated, please restart the application!" }
     }
 
     companion object {
-        private fun getClassNamesInPackages(packageNames: List<String>, classes: List<Class<*>>): List<String> {
-            val fqns: MutableList<String> = mutableListOf()
+        private const val TEMPLATE = "templates/RuntimeHintsReflectionTemplate.mustache"
 
-            classes.stream().map<String> { c: Class<*>? -> c!!.getName() + "::class" }
-                .forEach { e: String -> fqns.add(e) }
+        private fun getClassNamesInPackages(packageNames: List<String>, classes: List<Class<*>>): List<String> =
+            classes.map { "${it.name}::class" } +
+                    packageNames.flatMap { getClassNamesInPackage(it) }
 
-            packageNames.stream()
-                .map<List<String>> { packageName: String -> Companion.getClassNamesInPackage(packageName) }
-                .flatMap<String?> { obj: List<String> -> obj.stream() }
-                .forEach { e: String -> fqns.add(e) }
-
-            return fqns
-        }
-
-        private fun getClassNamesInPackage(packageName: String): List<String> {
-            val path = "src/main/java/" + packageName.replace('.', '/')
-            val classNames: MutableList<String> = mutableListOf()
-
-            try {
-                Files.walk(Paths.get(path)).use { paths ->
+        private fun getClassNamesInPackage(packageName: String) = runCatching {
+            Files
+                .walk(Paths.get("src/main/java/${packageName.replace('.', '/')}"))
+                .use { paths ->
                     paths
-                        .filter { path: Path -> Files.isRegularFile(path) }
-                        .filter { p: Path -> p.toString().endsWith(".java") }
-                        .forEach { p: Path ->
-                            classNames.add(
-                                packageName + "." + p.getFileName().toString().replace(".java", ".class")
-                            )
-                        }
+                        .filter(Files::isRegularFile)
+                        .filter { it.toString().endsWith(".java") }
+                        .map { "$packageName.${it.fileName.toString().removeSuffix(".java")}.class" }
+                        .toList()
                 }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-
-            return classNames
-        }
+        }.getOrDefault(emptyList())
     }
 }
